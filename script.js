d39cd2b3c198cb8b1ccbee855b30fdcf0b9599b5
RonnieMu34 <ronnie34mu@gmail.com>
2022-09-05 11:10:25 -0400

    Assign divs to variables

---
+++ script.js
@@ -0,0 +1,7 @@
+let display = document.getElementsByClassName("display");
+let add = document.getElementsByClassName("add-button");
+let subtract = document.getElementsByClassName("sub-button");
+let multiply = document.getElementsByClassName("mult-button");
+let divide = document.getElementsByClassName("div-button");
+let equals = document.getElementsByClassName("equals-button");
+let clear = document.getElementsByClassName("clear");
