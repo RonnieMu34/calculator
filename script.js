0beede07b80a15c2b6051fdc048f02f20b1d7458
RonnieMu34 <ronnie34mu@gmail.com>
2022-09-06 14:07:47 -0400

    Create nodelist for operator buttons

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let display = document.querySelector(".display");
-let add = document.querySelector(".add-button");
-let subtract = document.querySelector(".sub-button");
-let multiply = document.querySelector(".mult-button");
-let divide = document.querySelector(".div-button");
-let equals = document.querySelector(".equals-button");
+// let add = document.querySelector(".add-button");
+// let subtract = document.querySelector(".sub-button");
+// let multiply = document.querySelector(".mult-button");
+// let divide = document.querySelector(".div-button");
+// let equals = document.querySelector(".equals-button");
 let clear = document.querySelector(".clear");
 
 let firstNum = "";
 let secondNum = "";
 let operator = "";
 
+let opButtons = document.getElementsByClassName("operator");
 let numButton = document.getElementsByClassName("number");
 
 for (let i = 0; i < numButton.length; i++) {
